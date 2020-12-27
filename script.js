d6d7d4a1b28eda79bff9e7f6f3b2610e9500a5bf
haywah27 <hayleywahlroos15@gmail.com>
2020-12-27 12:23:43 -0800

    adding navbar and search box

---
+++ script.js
@@ -0,0 +1,21 @@
+// api call to weather api
+// form box/ search box for city
+// on click for search, return
+    // current weather: include-
+        // city name, 
+        // the date, 
+        // an icon representation of weather conditions, 
+        // the temperature, 
+        // the humidity, 
+        // the wind speed, 
+        // and the UV index: include-
+            // color that indicates whether the conditions are favorable, moderate, or severe
+
+    // 5 day weather outlook: include- 
+        // displays the date, 
+        // an icon representation of weather conditions, 
+        // the temperature, 
+        // the humidity
+
+// save city search as button to panel (local storage)
+// when the page is opened, its opened to the last searched city
