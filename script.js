209ca8ba8faad42c02dca8eb85454283275ef6de
haywah27 <hayleywahlroos15@gmail.com>
2021-01-01 23:15:43 -0800

    avoiding duplicates in local storage and button list

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// api call to weather api
-// form box/ search box for city
-// on click for search, return
-    // current weather: include-
-        // city name, 
-        // the date, 
-        // an icon representation of weather conditions, 
-        // the temperature, 
-        // the humidity, 
-        // the wind speed, 
-        // and the UV index: include-
-            // color that indicates whether the conditions are favorable, moderate, or severe
-
-    // 5 day weather outlook: include- 
-        // displays the date, 
-        // an icon representation of weather conditions, 
-        // the temperature, 
-        // the humidity
-
-// save city search as button to panel (local storage)
-// when the page is opened, its opened to the last searched city
-
 const apiKey = "9d892215aa42af9ae78d3f8d9778538a"
 
 const searchBtn = $(".searchBtn");
@@ -32,23 +10,21 @@ const userSearchDetails = $(".userSearchInfo");
 const buttonDump = $(".buttonDump");
 const forcastContain = $(".forcastContainer");
 const userSearchUV = $(".uvContainer ");
-
+const cityButton = $(".cityButton");
 
 var buttonList = [];
-var cityArr = [];
-
-var cityButton = $(".cityButton");
-
-
-
 
 localPost()
 clearList();
+$(".searchedCityColumn").hide();
 
 function localPost(){
     var cityHistory = JSON.parse(window.localStorage.getItem("city-name")) || [];
     console.log("this cart:",cityHistory)
+
+
     for (var i = 0; i < cityHistory.length; i++){
+ 
         var newButton = $("<button class='button is-fullwidth is-rounded cityButton'>").text(cityHistory[i]);
     
         $(buttonDump).append(newButton);
@@ -67,6 +43,7 @@ function localPost(){
 
 
 searchBtn.click(function(){
+
     const searchedCity = $(searchBox).val();
     console.log("this is the city entered:", searchedCity);
     postCityButton(searchedCity);
@@ -75,42 +52,30 @@ searchBtn.click(function(){
 
 });
 
-// function handleSearch() {
-//     const searchedCity = $(searchBox).val();
-//     console.log("this is the city entered:", searchedCity);
-//     postCityButton(searchedCity);
-//     nowWeather(searchedCity);
-//     forcast(searchedCity);
-    
-// }
 
-
+var fuck = [];
 function postCityButton(searchedCity){
-
     var capitalizeButton = searchedCity.charAt(0).toUpperCase() + searchedCity.slice(1);    
+
     var newButton = $("<button class='button is-fullwidth is-rounded cityButton'>").text(capitalizeButton);
-    
+
+    fuck.push(searchedCity);
+    console.log("fuck: ", fuck)
     buttonList.push(newButton);
-    cityArr.push(searchedCity);
+        
+    var cityHistory = JSON.parse(window.localStorage.getItem("city-name")) || [];
+    
+    if (cityHistory.indexOf(capitalizeButton) === -1){
+        cityHistory.push(capitalizeButton);
+        window.localStorage.setItem("city-name", JSON.stringify(cityHistory));
+        $(buttonDump).append(newButton);
+    }
 
     $(newButton).click(function() {
-        console.log("clicked",this);
         var inputSave = $(this).text();
         nowWeather(inputSave);
         forcast(inputSave);
-        console.log("value", inputSave);
-        
-     })
-
-
-    
-     var cityHistory = JSON.parse(window.localStorage.getItem("city-name")) || [];
-     cityHistory.push(capitalizeButton);
-     window.localStorage.setItem("city-name", JSON.stringify(cityHistory));
-
-    $(buttonDump).append(newButton);
-
-
+    })
 
 
 }
@@ -126,9 +91,12 @@ function clearList(){
 
 
 function nowWeather(searchedCity){
+    $(".searchedCityColumn").show();
     userSearchTitle.empty();
     userSearchIcon.empty();
     userSearchDetails.empty();
+    userSearchUV.empty();
+    $(".forcast-title").empty();
     forcastContain.empty();
    
     
@@ -137,7 +105,7 @@ function nowWeather(searchedCity){
         url: currentCity,
         method: "GET"
       }).then(function(response) {
-          
+
           var cityName = response.name;
           var cityIconCode = response.weather[0].icon;
           var iconURL = "http://openweathermap.org/img/w/" + cityIconCode + ".png";
@@ -159,9 +127,10 @@ function nowWeather(searchedCity){
 
 function currentUV(latitude, longitude){
     
-    userSearchUV.html($("<div class='box UVbox'></div>"));
+    $(".userSearchInfo").append($("<div class='box UVbox'></div>"));
     
-    $(".UVbox").html($('<div class="has-text-centered uvText"></div>'));
+    $(".UVbox").append($('<div class="has-text-centered uvText"></div>'));
+    
     
     const cityUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
 
@@ -201,6 +170,8 @@ function forcast(searchedCity){
         var tempArr = [];
         var humidArr = [];
       
+        var forcastTitle = $('<h3 class="title is-3">').text("5 Day Forcast");
+        $(".forcast-title").append(forcastTitle);
 
         for(var i = 0; i < listLength.length; i+= 8){
             console.log("forloop resonse:", listLength[i]);
